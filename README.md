08b1b5bb670844cf88e207c503c88311ea832e35
Hans Knecht <hknecht@huronconsultinggroup.com>
2021-04-16 08:38:10 -0700

    Initial xml handing examples

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # FeelThePowerShell
 Tester for Powershell items
+
+## Shared Library
+
+### XML Reading and updating
+Script to read and update an XML file. This is usefull for automation local installs independent of tools inside Visual Studio for applicaiotns. 
+
+**Reading in a file**
+
+To load a reaonable sized file the Get-Contect with casting to xml works well
+
+```powershell
+[xml]$xml = Get-Content $source
+```
+If you want to list out a specific element and their values in a file you can use XPath
+
+```powershell
+Select-Xml -Xml $xml -XPath '//author'
+```
+
+You could also use dot notation
+
+```powershell
+$xml.catalog.book.author
+```
+
+To search for a single node and its content
+```powershell
+($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author
+```
+
+To show the schema under a node, pipe to Get-Member the property membertype
+
+```powershell
+$xml.catalog.book | Get-Member -MemberType Property
+```
+
+For changing elements you navigate to the elment to reset the value. Calling save with the source file
+```powershell
+$xml.Save($source)
+```
+You use the same syntax to see an element with the additional assignment operator
+```powershell
+($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author = 'Eva, Corets'
+```
---
+++ XMLManipulation.ps1
@@ -0,0 +1,61 @@
+<#
+    .SYNOPSIS
+        This will install HAP locally
+    .DESCRIPTION
+        This will verify that the HAP default user is installed in the local database.
+        You may need to update the servername and instance name to work as expected.
+    .NOTES
+        Double check all variables prior to deployment
+
+    .EXAMPLE
+        .\InstallLocalHAP.ps1
+        
+#>
+BEGIN {
+    function SaveABackup($source) {
+        Copy-Item $source -Destination "$source.bak"
+    }
+    function ResetSource($source) {
+        Copy-Item "$source.bak" -Destination $source
+    }
+
+    function TraverseTheSource($source) {
+        [xml]$xml = Get-Content $source
+        Write-Host('You can select using the XPath pattern')
+        Write-Host(Select-Xml -Xml $xml -XPath '//author')
+        Write-Host
+
+        Write-Host('Or use dot notation')
+        Write-Host($xml.catalog.book.author)
+        Write-Host
+
+        Write-Host('to search for a single node and its content')
+        Write-Host(($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author)
+        Write-Host
+        
+        Write-Host('To Show the schema under a node pipe to Get-Member')
+        Write-Host($xml.catalog.book | Get-Member -MemberType Property)
+        Write-Host
+
+    }
+    function ChangeElements($source) {
+        [xml]$xml = Get-Content $source
+        Write-Host("Changing the value of an element is as simple as assiging it and calling save to the source")
+        Write-Host("Before: " + ($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author)
+        ($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author = 'Definetly not Eva Corets'
+        Write-Host("After: " + ($xml.catalog.book | Where-Object { $_.id -eq 'bk103'}).author)
+        $xml.Save($source)
+    }
+
+}
+PROCESS {
+    $source = './books.xml'
+    SaveABackup $source
+    TraverseTheSource $source
+    ChangeElements $source
+    ResetSource $source
+}
+
+END {
+
+}
